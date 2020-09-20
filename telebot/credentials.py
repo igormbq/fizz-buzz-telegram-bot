8060193aa65538e3fb90281ceda10ec1cd6e793b
igormbq <igormbq@gmail.com>
2020-09-20 13:59:25 -0300

    Initial commit

---
+++ telebot/credentials.py
@@ -0,0 +1,7 @@
+# bot_token = "here goes your access token from BotFather"
+# bot_user_name = "the username you entered"
+# URL = "the heroku app link that we will create later"
+
+bot_token = "919142306:AAF8FGRQ9SOrquBUFdq6i0463vdM8LwI2Q8"
+bot_user_name = "fizz_cloudia_bot"
+URL = "THE-HEROKU-APP-LINK"
