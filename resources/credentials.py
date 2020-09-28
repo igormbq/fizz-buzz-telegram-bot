942e338eaec06a33ca66c07ac707b480b77b428d
Igor Matheus Barbosa Quintana <57354688+igormbq@users.noreply.github.com>
2020-09-28 10:33:53 -0300

    Update credentials.py

--- resources/credentials.py
+++ resources/credentials.py
@@ -1,7 +1,3 @@
 # bot_token = "here goes your access token from BotFather"
 # bot_user_name = "the username you entered"
 # URL = "the app link that we will create later"
-
-bot_token = "919142306:AAF8FGRQ9SOrquBUFdq6i0463vdM8LwI2Q8"
-bot_user_name = "fizz_cloudia_bot"
-URL = "https://igormbq.pythonanywhere.com/"
